7540fe0f687342dc83f7c56d146e41120d0ed7f4
Danielle Bruce <you@example.com>
2024-07-02 10:02:54 -0400

    Connects to API and adds some styling

--- src/components/CurrentShows/CurrentShows.jsx
+++ src/components/CurrentShows/CurrentShows.jsx
@@ -1,21 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import './CurrentShows.scss'
 
 const CurrentShows = () => {
 
-    // const [shows, setShows] = useState[]
+    const [shows, setShows] = useState([])
+    const API_URL = import.meta.env.VITE_API_URL 
 
-    const getCurrentShows = () => {
-
+    const getCurrentShows = async() => {
+        const response = await axios(`${API_URL}/exhibitions`);
+        setShows(response.data)
     }
 
-    return (
-        <main>
-            <header>
-                <h1>Current Shows</h1>
-                <section>
+    useEffect(() => {
+        getCurrentShows()
+    }, [])
 
-                </section>
+    return (
+        <main className="home">
+            <header>
+                <h1 className="current-shows__title">Current Shows</h1>
             </header>
+            <section className="current-shows">
+                {
+                    shows.map((show) => {
+                        return (
+                            <article className="exhibition">
+                                <img className='exhibition__image' src={`${API_URL}/public/images/${show.show_image}`} />
+                                <div className="exhibition-info">
+                                    <h3 className="exhibition-info__title">{show.title} <span className="exhibition-info__artist">| {show.artist}</span></h3>
+                                    <p className="exhibition-info__place">{show.location}</p>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </section>
         </main>
     )
 }
